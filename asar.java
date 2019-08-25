3a2bc950c2fd4b2298894baaa082b7a379f6c67a
SouravBear8850 <53810519+SouravBear8850@users.noreply.github.com>
2019-08-25 17:10:28 +0530

    created ass4.java
    assignment 4

---
+++ asar.java
@@ -0,0 +1,13 @@
+
+public class ass3 {
+
+  public static void main(String[] args) {
+   int a=7,b=9;
+   System.out.println(a&b);
+   System.out.println(a|b);
+   System.out.println(a^b);
+   System.out.println(a<<2);
+   System.out.println(a>>2);
+  }
+
+}
